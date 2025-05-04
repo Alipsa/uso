9cdb52eb2e6ab38e08ee32d472a161d30afa9afe
per <per@alipsa.se>
2025-05-04 21:37:47 +0200

    improve installation

--- uso-core/src/main/script/installUso.sh
+++ uso-core/src/main/script/installUso.sh
@@ -1,6 +1,7 @@
-wget https://raw.githubusercontent.com/Alipsa/uso/refs/heads/main/uso-core/src/main/script/uso
+#!/usr/bin/env bash
+wget -q https://raw.githubusercontent.com/Alipsa/uso/refs/heads/main/uso-core/src/main/script/uso
 chmod +x uso
-wget https://raw.githubusercontent.com/Alipsa/uso/refs/heads/main/uso-core/src/main/script/usas
+wget -q https://raw.githubusercontent.com/Alipsa/uso/refs/heads/main/uso-core/src/main/script/usas
 chmod +x usas
 if [[ ! -f build.groovy ]]; then
   echo "project.with {
@@ -11,3 +12,4 @@ if [[ ! -f build.groovy ]]; then
   // target definitions goes here
 }" > build.groovy
 fi
+echo "Installation of uso and usas scripts completed! Edit build.groovy to add your project building details."
