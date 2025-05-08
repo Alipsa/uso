6bb338a4bcf8b0ea63bc2a0ba14a26907097c5b6
pernyf <per.nyfelt@nordnet.se>
2025-05-08 10:06:05 +0200

    reusable function for example builds

--- buildAll.sh
+++ buildAll.sh
@@ -2,6 +2,21 @@
 set -e
 cd "$(dirname "$0")"
 
+function buildExample() {
+  pushd "$1"
+    echo ""
+    echo "********************************"
+    echo "Building $(basename $PWD) using uso"
+    echo "********************************"
+    ./uso clean test
+    echo ""
+    echo "*********************************"
+    echo "Building $(basename $PWD) using usas"
+    echo "*********************************"
+    ./usas clean test
+  popd
+}
+
 pushd uso-core
   echo ""
   echo "********************"
@@ -9,18 +24,8 @@ pushd uso-core
   echo "********************"
   ./build deployLocal
 popd
-pushd examples/simpleExample
-  echo ""
-  echo "********************************"
-  echo "Building simpleExample using uso"
-  echo "********************************"
-  ./uso clean compile
-  echo ""
-  echo "*********************************"
-  echo "Building simpleExample using usas"
-  echo "*********************************"
-  ./usas clean compile
-popd
+buildExample examples/simpleExample
+# buildExample examples/dependencies
 echo ""
 echo "*****"
 echo "Done!"
