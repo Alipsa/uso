d6b275f910796430599288c1fabd5cac6c32f5e5
pernyf <per.nyfelt@nordnet.se>
2025-05-20 14:45:35 +0200

    WIP Layout task

---
+++ examples/multimodule/readme.md
@@ -0,0 +1,5 @@
+# Multi-module example
+This project is NOT yet functional. It is a work in progress.
+
+TODO: 
+- https://exubero.com/ant/dependencies/ has some good ideas
---
+++ uso-tasks/src/main/groovy/se/alipsa/uso/tasks/Layout.groovy
@@ -0,0 +1,22 @@
+package se.alipsa.uso.tasks
+
+/**
+ * This task creates some standard layout for a project and defined properties
+ * pointing to each directory.
+ * <p>
+ * The layout type can be one of:
+ * <ul>
+ *  <li>maven</li>
+ *  <li>gradle</li>
+ *  <li>simple</li>
+ * </ul>
+ * </p>
+ * The following properties are defined:
+ * TODO: see
+ * https://ant.apache.org/manual/properties.html
+ * https://web.archive.org/web/20150520200505/https://docs.codehaus.org/display/MAVENUSER/MavenPropertiesGuide
+ * https://docs.gradle.org/current/dsl/org.gradle.api.Project.html?_ga=2.142209076.745539784.1747744750-1766038757.1739351589&_gl=1*13dw785*_gcl_au*OTQwNzQ5NTAyLjE3NDU4MzMxMjk.*_ga*MTc2NjAzODc1Ny4xNzM5MzUxNTg5*_ga_7W7NC6YNPT*czE3NDc3NDQ3NTAkbzQ2JGcwJHQxNzQ3NzQ0NzUwJGo2MCRsMCRoMCRkb1BoUHdpbVgwTlBpN0pDOHhORzhiazUyQnpYWGRLeHlyQQ..#org.gradle.api.Project:buildDir
+ *
+ */
+class Layout {
+}
